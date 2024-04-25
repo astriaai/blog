4c92ed1a7cf9b018efe8237c2177df6e9b114302
Alon Burg <burgalon@gmail.com>
2024-04-25 23:32:06 +0300

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,163 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/vsDark');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Astria blog',
+  tagline: 'Learn about Fine-tuning generative AI image personalization',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://blog.astria.ai',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'astriaai', // Usually your GitHub org/user name.
+  projectName: 'astria-blog', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  plugins: [
+    // 'docusaurus-plugin-image-zoom',
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarCollapsed: false,
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'All our posts',
+          blogDescription: 'Learn about fine-tuning and AI photography with consistent characters and high quality images.',
+
+          path: 'blog',
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-S3TGRCPBP8',
+          anonymizeIP: false, // Should IPs be anonymized?
+        }
+      }),
+    ],
+  ],
+
+  themeConfig:
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      zoom: {
+        selector: '.markdown img',
+        background: {
+          light: 'rgb(255, 255, 255)',
+          dark: 'rgb(50, 50, 50)'
+        },
+        config: {
+          // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+        }
+      },
+      image: 'img/high-quality-finetuning.jpg',
+      navbar: {
+        title: 'Astria blog',
+        logo: {
+          alt: 'Astria',
+          src: 'img/logo@2x.png',
+        },
+        items: [
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Use cases',
+          // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://www.astria.ai/',
+            label: 'Home',
+            // position: 'right',
+          },
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Use cases',
+          //       to: '/docs/category/use-cases',
+          //     },
+          //     {
+          //       label: 'Features',
+          //       to: '/docs/category/features',
+          //     },
+          //   ],
+          // },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/MtW9gBgsMX',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/Astria_AI',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Gallery',
+                to: 'https://www.astria.ai/gallery',
+              },
+              {
+                label: 'Community examples',
+                href: 'https://astria.ai/community',
+              },
+              {
+                label: 'Home',
+                href: 'https://astria.ai/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Astria, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
